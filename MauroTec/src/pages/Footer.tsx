d163c03ba50e0eb53dc79193acc119eb5519a7a7
Nilo Araújo <niloaraujodacosta@gmail.com>
2025-03-08 14:21:28 -0300

    responsividade da parte de mapa

--- MauroTec/src/pages/Footer.tsx
+++ MauroTec/src/pages/Footer.tsx
@@ -4,7 +4,16 @@ import './Footer.css'
 function Footer(){
     
     return (
-        <div className='Footer'></div>
+        <div className='Footer'>
+            <div className='linha-principal'>
+                <div className="coluna-imagens">
+                    <div className='logo-maurotec'>
+                        <img src="../assets/images/imagensServicos/logo2.jpg" alt="" />
+                    </div>
+                    <div className='redes-sociais'></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
